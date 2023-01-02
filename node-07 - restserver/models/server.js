6fceb0e0a689a35f1c0c9e61ce7a82af57278789
joaquin grosso <joagrosso@hotmail.com>
2023-01-02 13:50:08 -0300

    servers

---
+++ node-07 - restserver/models/server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas
+        this.routes();
+    };
+
+    middlewares(){
+        //CORS
+        this.app.use( cors() );
+
+        //Lectura y parseo del body
+        this.app.use( express.json() );
+        
+        //Directorio publico 
+        this.app.use(express.static('public'))
+    };
+
+    routes(){
+        this.app.use('/api/user', require('../routes/user'));
+    };
+
+    listen(){
+        this.app.listen(this.port);
+    };
+
+}
+
+
+module.exports = Server;
